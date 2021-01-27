622470e2532025639b2766e120fd763a5c8295ff
Release Standard <releasestandard@netc.eu>
2021-01-27 19:58:53 +0100

    Possibility to extend easily

--- app/src/main/java/com/releasestandard/scriptmanager/BashInterface.java
+++ app/src/main/java/com/releasestandard/scriptmanager/BashInterface.java
@@ -10,20 +10,27 @@ public class BashInterface {
     // these files are used only for this classe so we don't use the StorageManager //
     private static String SUFFIX_PID = ".pid";
     private static String SUFFIX_ARG = ".arg";
+    private static String SUFFIX_CALLED = ".status";
     private String pidFile = null;
+    private String functNameFile = null;
     private String arg0 = null;
     private String arg1 = null;
     private Boolean isWrappScriptCalled = false;
-    private HashMap<String,String> events =  new HashMap<String, String>() {{
-        put("msg_recv", "USR1");
-        put("msg_send", "USR2");
+    private String signal = "USR1";
+
+    // API definition
+    // event -> function name associated
+    public HashMap<String,String> API = new HashMap<String, String>() {{
+
+        put("smsReceived","smsReceived"); // $1 : from $2 : body
+
     }};
 
     public void dump() { Logger.debug(dump("")); }
     public String dump(String off) {
             String noff = off + "\t";
             return off + "BashInterface {\n"+
-                    noff + "events=" + events.size() + "\n" +
+                    noff + "signal="+signal+"\n" +
                     off + "}\n"
                     ;
     }
@@ -41,18 +48,20 @@ public class BashInterface {
         pidFile = out + SUFFIX_PID;
         arg0 = out + SUFFIX_ARG + "0";
         arg1 = out + SUFFIX_ARG + "1";
+        functNameFile = out + SUFFIX_CALLED;
 
         String header = "" +
                  "       _scriptmanager_pidf=\"" + pidFile + "\" ;     \n" +
-                "        _scriptmanager_is_trap_set=false ;              \n" +
-                "        _scriptmanager_SIG_msg_recv=\""+events.get("msg_recv")+"\" ;         \n" +
+                "        _scriptmanager_SIG=\""+signal+"\" ;         \n" +
                 "\n" +
-                "handle_msg_recv () { \n" +
-                "         _scriptmanager_is_trap_set=true ; \n" +
-                "         msg=\"\\$(cat "+arg0+")\"      ;             \n" +
-                "         tel=\"\\$(cat "+arg1+")\" ;            \n" +
-                "         trap \"$1 \\\"$msg\\\" \\\"$tel\\\"\" $_scriptmanager_SIG_msg_recv ;  \n" +
+                "events_interface () { \n" +
+                "         arg0=\"$(cat "+arg0+")\"      ;             \n" +
+                "         arg1=\"$(cat "+arg1+")\" ;                  \n" +
+                "         functname=\"$(cat "+ functNameFile +")\" ;     \n" +
+                "         type $functname &> /dev/null && " + // if user has defined a callback
+                "          $functname \"$arg0\" \"$arg1\" ;                  \n" +
                 "}\n" +
+                "trap \"events_interface\" $_scriptmanager_SIG; \n" +
                 "\n" +
                 "echo \"$$\" > " + pidFile + " ; \n" +
                 "# \n" +
@@ -67,7 +76,7 @@ public class BashInterface {
                 "#\n" +
                 "#\n" +
                 "#\n" +
-                "while $_scriptmanager_is_trap_set ; do\n" +
+                "while true ; do\n" +
                 "\tsleep 100 &\n" +
                 "\twait $!\n" +
                 "done\n";
@@ -99,10 +108,11 @@ public class BashInterface {
         Logger.debug("triggerRecvMsg,from="+from+",body="+body);
         String cmd = "" +
                 "" +
+                "echo \"" + API.get("smsReceived") + "\" > " + functNameFile + " && " +
                 "echo \"" + from + "\" > " + arg0 + " && " +
                 "echo \"" + body + "\" > " + arg1 + " && " +
                 "" +
-                "kill -s " + events.get("msg_recv") + " $(cat " + pidFile + ");";
+                "kill -s " + signal + " $(cat " + pidFile + ");";
         Logger.debug(cmd);
         Shell._execCmd(cmd);
     }
