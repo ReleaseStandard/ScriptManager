82a45066e62c5852120577977e4ff7e9aac54098
Release Standard <releasestandard@netc.eu>
2021-02-04 19:14:33 +0100

    Automatic compat detection

--- scripts/checkcompat.sh
+++ scripts/checkcompat.sh
@@ -3,4 +3,15 @@
 # If class is compat x then all methods are compat level x.
 # W: For compat not recursive checks: e.g. class A extends B (B compat 10, A compat 1)
 #    ~All basic blocks of the class are compat level x
-find app/src/main/java/ -name "*.java" -exec bash -c 'grep "* compat" {} | sed "s/^ \+\* compat //g"' \; |sort -u -n|tail -n 1
+
+
+t="/tmp/t";
+g="./app/build.gradle";
+l=$(find app/src/main/java/ -name "*.java" -exec bash -c 'grep "* compat" {} | sed "s/^ \+\* compat //g"' \; |sort -u -n|tail -n 1)
+echo "$l";
+
+if [ "$1" = "write" ] ; then
+	echo "Modify build.gradle";
+	sed 's/\(minSdkVersion \+\)[0-9]\+/\1'${l}'/g' $g > $t;
+	mv $t $g;
+fi
